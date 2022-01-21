ef076e6a8eca711c0cb220ead102aef3225cf193
EmmaHammar <emma.s.hammar@gmail.com>
2022-01-21 11:51:11 +0100

    testing

--- logo-position.js
+++ logo-position.js
@@ -1,11 +1,11 @@
 let pickedFile = false;
 let pickedPosition = false; //TODO add this
-let cssTextImgTag;
-let cssTextImgContainer;
+let cssTextImgTag; //byt namn till cssTextLogo
+let cssTextImgContainer; //byt namn till cssTextLogoContainer
 let srcPath;
 let url;
 let logoContainer = document.getElementById("logoContainer");
-let img;
+let logo;
 
 document.getElementById("file-picker").addEventListener("change", (evt) => {
     emptyTextareas();
@@ -14,7 +14,7 @@ document.getElementById("file-picker").addEventListener("change", (evt) => {
     srcPath = "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"; //static
     console.log("srcPath", srcPath);
 
-    img = document.createElement("img");
+    logo = document.createElement("img");
     let file = evt.target.files[0];
 
     //show errorMsg if file is not an img
@@ -25,42 +25,41 @@ document.getElementById("file-picker").addEventListener("change", (evt) => {
     pickedFile = true;
 
     //add src attr to img tag
-    img.src = srcPath; 	
-    console.log("img", img);
+    logo.src = srcPath; 	
+    console.log("logo", logo);
 });
 
+//create css strings to logo and container elements depending on picked position in dropdown
 const addStyleAttr = (selectElementValue) => {
     switch (selectElementValue) {
         case "top-left":
-            //create css strings to tag and container
             cssTextImgTag = "width: 50%";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start;";
-            
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-start;";
         break;
         case "top-center": 
-            cssTextImgTag = "display: block; margin-left: auto; margin-right: auto; width: 50%;";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start;";
+            cssTextImgTag = "width: 50%";
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: center;";
         break;
         case "top-right": 
-            cssTextImgTag = "width: 50%; position: absolute; right: 0px;";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; position: relative;";
+            cssTextImgTag = "width: 50%";
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-end;";
         break;
         case "bottom-left":
             cssTextImgTag = "width: 50%;";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end;";
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-start;";
         break;
         case "bottom-center": 
-            cssTextImgTag = "display: block; margin-left: auto; margin-right: auto; width: 50%;";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end;";
+            cssTextImgTag = "width: 50%;";
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: center;";
         break;
         case "bottom-right": 
-            cssTextImgTag = "width: 50%; position: absolute; right: 0px;";
-            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; position: relative;";
+            cssTextImgTag = "width: 50%;";
+            cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-end;";
         break;
         default:
             break;
     };
-    img.style.cssText = cssTextImgTag;
+    logo.style.cssText = cssTextImgTag;
     logoContainer.style.cssText = cssTextImgContainer;
 };
 
@@ -81,21 +80,34 @@ document.getElementById("upload-btn").addEventListener("click", (evt) => {
     
         addStyleAttr(selectElementValue);
 
-        let imgToString = img.outerHTML;
+        
+        //logoToString should be both logoContainer div and logo img tag -> toString -> print in textArea - TODO: START HERE!!       
+        let logoToString = logo.outerHTML;
+        console.log("logo", typeof logo);
+        let logoContainerToString = logo.closest("div");
+        //istället för img också ha imgContainer incl img
+        console.log("logoContainer", logoContainer);
+        console.log("logoContainerToString parent and child?", logoContainerToString);
+        //https://stackoverflow.com/questions/27659580/get-the-parent-element-html-along-with-child-content/27659639
+
+        
+
 
         //check if picked position is top or bottom textarea: 
         let isTop = selectElementValue.includes("top");
         if (isTop) {
             //print in top 
-            document.getElementById("top").innerHTML = imgToString;
+            document.getElementById("top").innerHTML = logoToString; //change to logoContainerToString
+
+
         } else {
             //print in bottom 
-            document.getElementById("bottom").innerHTML = imgToString;
+            document.getElementById("bottom").innerHTML = logoToString; //change to logoContainerToString
         };
 
     } else {
         console.log("errorMsg = Du måste välja vilken fil du vill ladda upp.");
     };
 
-    logoContainer.appendChild(img); 
+    logoContainer.appendChild(logo); 
 });
