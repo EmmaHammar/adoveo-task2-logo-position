baaf22a86ada601e4c1fca73d35007d9ae8b13e7
EmmaHammar <emma.s.hammar@gmail.com>
2022-01-22 14:59:35 +0100

    prints right cssText and prints in right textArea

--- logo-position.js
+++ logo-position.js
@@ -1,157 +1,110 @@
-let pickedFile = false; //change to isPickedFile
-let pickedPosition = false; //change to isPickedPosition
-let cssTextImgTag; //change to cssTextLogo
-let cssTextImgContainer; //change to cssTextLogoContainer
+let tagStr;
 let srcPath;
-let url;
-let logoContainer = document.getElementById("logo-container");
-let logo;
-let logoPositionDropDown = document.querySelector("#logo-position"); //change to positionPicker
-let pickedLogoPosition; 
+let logoContainerStyle = "";
+let logoStyle = "";
+let isUpload = false; 
+let isPickedFile = false; 
+let positionPicker = document.querySelector("#logo-position");
+let selectedPosition;
 
-//change name to logoPosition (insted of selectElementValue)
-let selectElementValue = document.querySelector("#logo-position").value; 
-let logoToString; //used when check isTop
-
-//create css strings to logo and container elements depending on picked position in dropdown
-function addStyleAttr(selectElementValue) {
-    switch (selectElementValue) {
-        case "top-left":
-            cssTextImgTag = "width: 20%; margin-top: 4%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-start;";
-            cssTextImgContainer = "text-align:left;";
-        break;
-        case "top-center": 
-            cssTextImgTag = "width: 20%; margin-top: 4%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: center;";
-            cssTextImgContainer = "text-align:center;";
-
-        break;
-        case "top-right": 
-            cssTextImgTag = "width: 20%; margin-top: 4%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-end;";
-            cssTextImgContainer = "text-align:right;";
-
-        break;
-        case "bottom-left":
-            // cssTextImgTag = "width: 50%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-start;";
-            cssTextImgTag = "width: 20%; position: absolute; bottom: 0px; margin-bottom: 4%;";
-            cssTextImgContainer = "text-align:left;";            
-        break;
-        case "bottom-center": 
-            // cssTextImgTag = "width: 50%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: center;";
-            cssTextImgTag = "width: 20%; position: absolute; bottom:0; margin-bottom: 4%;";
-            cssTextImgContainer = "text-align:center; padding-right:10%;";
-        break;
-        case "bottom-right": 
-            // cssTextImgTag = "width: 50%;";
-            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-end;";
-            cssTextImgTag = "width: 20%; position: absolute; bottom: 0px;margin-bottom: 4%;";
-            cssTextImgContainer = "text-align: right; padding-right: 12%;";
-
-        break;
-        default:
-            break;
-    };
-    logo.style.cssText = cssTextImgTag;
-    logoContainer.style.cssText = cssTextImgContainer;
-};
+//endast för kom ihåg, ta bort var deklaration sen: 
+let filePicker = document.getElementById("file-picker");
+// textarea top 
+// textarea bottom
+// previewContainer
+// errorMsg
 
 function emptyTextareas() {
     document.getElementById("top").innerHTML = "";
     document.getElementById("bottom").innerHTML="";
 };
 
-function checkIfTop(selectElementValue, logoToString) {
-    // console.log("hej från checkIfTop. selectElementValue:", selectElementValue, "logoToString", logoToString);
-    let isTop = selectElementValue.includes("top");
-    if (isTop) {
-        //print in top 
-        document.getElementById("top").innerHTML = logoContainerToString; //change to logoContainerToString
-    } else {
-        //print in bottom 
-        document.getElementById("bottom").innerHTML = logoContainerToString; //change to logoContainerToString
+function updateStyle(selectedPosition) {
+    switch (selectedPosition) {
+        case "top-left":
+            logoContainerStyle = "text-align:left;";
+            logoStyle = "width: 20%; margin-top: 4%; margin-left: 2%;";
+        break;
+        case "top-center": 
+            logoContainerStyle = "text-align:center;";
+            logoStyle = "width: 20%; margin-top: 4%;";
+        break;
+        case "top-right": 
+            logoContainerStyle = "text-align:right;";
+            logoStyle = "width: 20%; margin-top: 4%; margin-right: 2%;";
+        break;
+        case "bottom-left":
+            logoContainerStyle = "text-align:left;";      
+            logoStyle = "width: 5%; position: absolute; bottom: 0px; margin-bottom: 2%; margin-left: 0.5%";
+            
+        break;
+        case "bottom-center": 
+            logoContainerStyle = "text-align:center; padding-right:20%;";
+            logoStyle = "width: 5%; position: absolute; bottom:0; margin-bottom: 2%;";
+        break;
+        case "bottom-right": 
+            logoContainerStyle = "text-align: right; padding-right: 12%;";
+            logoStyle = "width: 5%; position: absolute; bottom: 0px; margin-bottom: 2%; margin-left: -3%;";
+        break;
+        default:
+        break;
     };
 };
 
-document.getElementById("upload-btn").addEventListener("click", function(evt) {
+function checkTopOrBottom(selectedPosition) {
     emptyTextareas();
-    logoContainerToString="";
-    logoToStringAndDivEndtag="";
+    let isTop = selectedPosition.includes("top");
 
-    if (pickedFile === true) {        
-        if (pickedPosition === true) {
-            addStyleAttr(pickedLogoPosition);
-        } else {
-            addStyleAttr(selectElementValue);
-        };
-
-        //KVAR logoContainerToString töms ej korrekt när man ändrar i dropdown samt vid klick upload btn: 
-        //string logoContainer tag and logo tag (print in textArea)
-        logoContainerToString = logoContainer.outerHTML;
-        logoContainerToString = logoContainerToString.split("</div>"); //split div start tag and end tag into arr
-        logoToString = logo.outerHTML;
-        let logoToStringAndDivEndtag = logoToString+"</div>"; //add div end tag to img tag
-        logoContainerToString = logoContainerToString[0] + logoToStringAndDivEndtag; //add div start tag to 
-        console.log("logoContainerToString:",  logoContainerToString);
-        console.log("logoContainerToString[0]:",  logoContainerToString[0]);
-        console.log("logoToStringAndDivEndtag", logoToStringAndDivEndtag);
-
-
-        //check if picked position is top or bottom textarea: 
-        if (pickedPosition === true) {
-            checkIfTop(pickedLogoPosition, logoToString);
-            logoContainer.innerHTML = logoContainerToString;
-        } else {
-            checkIfTop(selectElementValue, logoToString);
-            logoContainer.innerHTML = logoContainerToString;
-        };
-
+    if (isTop) {
+        //print in top 
+        document.getElementById("top").innerHTML = tagStr; 
     } else {
-        console.log("errorMsg = Du måste välja vilken fil du vill ladda upp.");
+        //print in bottom 
+        document.getElementById("bottom").innerHTML = tagStr; 
     };
-});
+};
+function previewLogo() {
+    // console.log("previewLogo()");
+};
 
 document.getElementById("file-picker").addEventListener("change", function(evt) {
+    isPickedFile = true; 
+
     emptyTextareas();
-
-    srcPath = evt.target.value; //get dynamically
-    // srcPath = "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"; //static
-    srcPath = "https://logos-download.com/wp-content/uploads/2020/07/Arvid_Nordquist_Logo.png";
-
-    logo = document.createElement("img");
-    let file = evt.target.files[0];
-
-    //show errorMsg if file is not an img
-    if (file.type && !file.type.startsWith('image/')) {
-        console.log('show errorMsg: file is not an image.', file.type, file);
-    return;
-    };
-    pickedFile = true;
-
-    //add src attr to img tag
-    logo.src = srcPath; 	
+    
+    // srcPath = evt.target.value; //get dynamically
+    srcPath = "https://logos-download.com/wp-content/uploads/2020/07/Arvid_Nordquist_Logo.png"; //static
 });
 
-logoPositionDropDown.addEventListener("change", function(evt) {
-    emptyTextareas(); 
+document.getElementById("upload-btn").addEventListener("click", function(evt) {
+    //empty previewContainer
+    document.getElementById("preview-container").innerHTML = "";
 
-    pickedLogoPosition = evt.target.value;
-    // console.log("pickedLogoPosition in dropdown:", pickedLogoPosition);
+    if (isPickedFile === true) {
+        isUpload = true;
 
-    if (pickedFile === true) {
-        // console.log("printa ändring i bild med position:", pickedLogoPosition);
-        addStyleAttr(pickedLogoPosition);
-        pickedPosition = true;
+        if (isUpload === true) {
+            //get selected position
+            selectedPosition = document.querySelector("select").value;
+            // console.log("visar rätt selectedPosition?", selectedPosition);
 
-        if (logoToString !== undefined) {
-            checkIfTop(pickedLogoPosition, logoToString);
+            updateStyle(selectedPosition);
+
+            tagStr = `<div id="logoContainer" style="${logoContainerStyle}"><img id="logo" src="${srcPath}" style="${logoStyle}"></div>`;
+            console.log("tagStr visar rätt:", tagStr);
+
+            checkTopOrBottom(selectedPosition);
+            previewLogo();
         } else {
-            console.log("du måste ladda upp bild för att kunna se - ej ha som errromsg utan bara för nu");
+            console.log("Error: Vänligen ladda upp en bildfil först, därefter kan du positionera bilden.");
         };
+
+
+            
     } else {
-        console.log("errorMsg: Du måste välja en fil innan du kan välja position");
+        console.log("Error: Vänligen välj en fil du vill ladda upp.");
     };
+    // isPickedFile = false;
 });
+
+
--- slask.js
+++ slask.js
@@ -44,3 +44,171 @@
         //     //create styleAttr + add to imgElement
         //     addStyleAttr(selectElementValue);
         // };
+
+
+
+//backup
+
+
+
+
+
+let pickedFile = false; //change to isPickedFile
+let pickedPosition = false; //change to isPickedPosition
+let cssTextImgTag; //change to cssTextLogo
+let cssTextImgContainer; //change to cssTextLogoContainer
+// let srcPath;
+let url;
+let logoContainer = document.getElementById("logo-container");
+let logo;
+let logoPositionDropDown = document.querySelector("#logo-position"); //change to positionPicker
+let pickedLogoPosition; 
+
+//change name to logoPosition (insted of selectElementValue)
+let selectElementValue = document.querySelector("#logo-position").value; 
+let logoToString; //used when check isTop
+
+//create css strings to logo and container elements depending on picked position in dropdown
+function addStyleAttr(selectElementValue) {
+    switch (selectElementValue) {
+        case "top-left":
+            cssTextImgTag = "width: 20%; margin-top: 4%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-start;";
+            cssTextImgContainer = "text-align:left;";
+        break;
+        case "top-center": 
+            cssTextImgTag = "width: 20%; margin-top: 4%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: center;";
+            cssTextImgContainer = "text-align:center;";
+
+        break;
+        case "top-right": 
+            cssTextImgTag = "width: 20%; margin-top: 4%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-start; justify-content: flex-end;";
+            cssTextImgContainer = "text-align:right;";
+
+        break;
+        case "bottom-left":
+            // cssTextImgTag = "width: 50%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-start;";
+            cssTextImgTag = "width: 20%; position: absolute; bottom: 0px; margin-bottom: 4%;";
+            cssTextImgContainer = "text-align:left;";            
+        break;
+        case "bottom-center": 
+            // cssTextImgTag = "width: 50%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: center;";
+            cssTextImgTag = "width: 20%; position: absolute; bottom:0; margin-bottom: 4%;";
+            cssTextImgContainer = "text-align:center; padding-right:10%;";
+        break;
+        case "bottom-right": 
+            // cssTextImgTag = "width: 50%;";
+            // cssTextImgContainer = "border: 1px solid black; width:390px; height:844px; display:flex; align-items: flex-end; justify-content: flex-end;";
+            cssTextImgTag = "width: 20%; position: absolute; bottom: 0px;margin-bottom: 4%;";
+            cssTextImgContainer = "text-align: right; padding-right: 12%;";
+
+        break;
+        default:
+            break;
+    };
+    logo.style.cssText = cssTextImgTag;
+    logoContainer.style.cssText = cssTextImgContainer;
+};
+
+function emptyTextareas() {
+    document.getElementById("top").innerHTML = "";
+    document.getElementById("bottom").innerHTML="";
+};
+
+function checkIfTop(selectElementValue, logoToString) {
+    // console.log("hej från checkIfTop. selectElementValue:", selectElementValue, "logoToString", logoToString);
+    let isTop = selectElementValue.includes("top");
+    if (isTop) {
+        //print in top 
+        document.getElementById("top").innerHTML = logoContainerToString; //change to logoContainerToString
+    } else {
+        //print in bottom 
+        document.getElementById("bottom").innerHTML = logoContainerToString; //change to logoContainerToString
+    };
+};
+
+document.getElementById("upload-btn").addEventListener("click", function(evt) {
+    emptyTextareas();
+    logoContainerToString="";
+    logoToStringAndDivEndtag="";
+
+    if (pickedFile === true) {        
+        if (pickedPosition === true) {
+            addStyleAttr(pickedLogoPosition);
+        } else {
+            addStyleAttr(selectElementValue);
+        };
+
+        //KVAR logoContainerToString töms ej korrekt när man ändrar i dropdown samt vid klick upload btn: 
+        //string logoContainer tag and logo tag (print in textArea)
+        logoContainerToString = logoContainer.outerHTML;
+        logoContainerToString = logoContainerToString.split("</div>"); //split div start tag and end tag into arr
+        logoToString = logo.outerHTML;
+        let logoToStringAndDivEndtag = logoToString+"</div>"; //add div end tag to img tag
+        logoContainerToString = logoContainerToString[0] + logoToStringAndDivEndtag; //add div start tag to 
+        console.log("logoContainerToString:",  logoContainerToString);
+        console.log("logoContainerToString[0]:",  logoContainerToString[0]);
+        console.log("logoToStringAndDivEndtag", logoToStringAndDivEndtag);
+
+
+        //check if picked position is top or bottom textarea: 
+        if (pickedPosition === true) {
+            checkIfTop(pickedLogoPosition, logoToString);
+            logoContainer.innerHTML = logoContainerToString;
+        } else {
+            checkIfTop(selectElementValue, logoToString);
+            logoContainer.innerHTML = logoContainerToString;
+        };
+
+    } else {
+        console.log("errorMsg = Du måste välja vilken fil du vill ladda upp.");
+    };
+});
+
+//körs när
+document.getElementById("file-picker").addEventListener("change", function(evt) {
+    console.log("klick?");
+    emptyTextareas();
+
+    srcPath = evt.target.value; //get dynamically
+    // srcPath = "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"; //static
+    srcPath = "https://logos-download.com/wp-content/uploads/2020/07/Arvid_Nordquist_Logo.png";
+
+    logo = document.createElement("img");
+    let file = evt.target.files[0];
+
+    //show errorMsg if file is not an img
+    if (file.type && !file.type.startsWith('image/')) {
+        console.log('show errorMsg: file is not an image.', file.type, file);
+    return;
+    };
+    pickedFile = true;
+
+    //add src attr to img tag
+    logo.src = srcPath; 	
+});
+
+logoPositionDropDown.addEventListener("change", function(evt) {
+    emptyTextareas(); 
+
+    pickedLogoPosition = evt.target.value;
+    // console.log("pickedLogoPosition in dropdown:", pickedLogoPosition);
+
+    if (pickedFile === true) {
+        // console.log("printa ändring i bild med position:", pickedLogoPosition);
+        addStyleAttr(pickedLogoPosition);
+        pickedPosition = true;
+
+        if (logoToString !== undefined) {
+            checkIfTop(pickedLogoPosition, logoToString);
+        } else {
+            console.log("du måste ladda upp bild för att kunna se - ej ha som errromsg utan bara för nu");
+        };
+    } else {
+        console.log("errorMsg: Du måste välja en fil innan du kan välja position");
+    };
+});
